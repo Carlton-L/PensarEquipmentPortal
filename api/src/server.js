e8c44dc09ade875d3f650bc2728ec8497282d51b
Carlton Lindsay <carlton.l@outlook.com>
2021-04-14 19:42:20 -0700

    Update server.js

--- api/src/server.js
+++ api/src/server.js
@@ -1,51 +1,11 @@
 const express = require("express");
-const {
-  ApolloServer,
-  AuthenticationError,
-  SchemaDirectiveVisitor,
-} = require("apollo-server-express");
-const { defaultFieldResolver } = require("graphql");
+const { ApolloServer } = require("apollo-server-express");
 const msal = require("@azure/msal-node");
 
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const { getUser } = require("./auth");
 const { config } = require("/config/MSAL");
-
-// Checks that the user is authenticated
-class AuthenticationDirective extends SchemaDirectiveVisitor {
-  visitObject(object) {
-    this.ensureFieldsWrapped(object);
-  }
-
-  visitFieldDefinition(field, details) {
-    this.ensureFieldsWrapped(details.objectType);
-  }
-
-  ensureFieldsWrapped(objectType) {
-    // Mark the GraphQLObjectType object to avoid re-wrapping
-    if (objectType._authFieldsWrapped) return;
-    objectType._authFieldsWrapped = true;
-
-    const fields = objectType.getFields();
-
-    Object.keys(fields).forEach((fieldName) => {
-      const field = fields[fieldName];
-      const { resolve = defaultFieldResolver } = field;
-      field.resolve = (root, args, context, info) => {
-        const user = context.user;
-        console.log("Authenticating...");
-        if (!user) {
-          throw new AuthenticationError(
-            "Access Denied: Authentication Failed."
-          );
-        }
-
-        return resolve.apply(this, args);
-      };
-    });
-  }
-}
 
 const server = new ApolloServer({
   typeDefs,
