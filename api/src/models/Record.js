02bfbec07b261b3e20655644c7a54b2c412e516d
Carlton Lindsay <carlton.l@outlook.com>
2021-04-20 14:00:39 -0700

    Update Record.js

--- api/src/models/Record.js
+++ api/src/models/Record.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const RecordSchema = mongoose.Schema({
   equipment: {
-    type: ObjectId,
+    type: mongoose.ObjectId,
     required: true,
   },
   user: {
