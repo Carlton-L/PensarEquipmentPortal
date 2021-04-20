7f59b04f706e12a3e47892e7cb9f2efb14cc198f
Carlton Lindsay <carlton.l@outlook.com>
2021-04-20 14:00:44 -0700

    Update resolvers.js

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,7 +1,38 @@
+const { getDescription } = require("graphql");
+
 module.exports = {
   Query: {
-    equipmentById(_, __, ___) {
-      return { id: "dfnsjdfns" };
+    equipmentById(_, { _id }, { models: { Equipment } }) {
+      return Equipment.findOne({ _id: _id }).then((equipment) => {
+        return {
+          ...equipment,
+          status,
+          calStatus,
+          log,
+          schedule,
+        };
+      });
+    },
+  },
+  Mutation: {
+    uploadImage(_, { url }, context) {},
+  },
+  Equipment: {
+    status({ id }) {
+      // Look for current log (checkOut date but no checkIn date)
+      // const log = Record.findOne({ _id: id, checkIn: null, checkOut: { $exists: true }})
+      const log = true;
+      if (log) {
+        return "UNAVAILABLE";
+      }
+
+      // Look for current reservation
+      // const reservation = Record.findOne({ _id: id, start: { $lt: Date.now() }, end: { $gt: Date.now()}})
+      // if (reservation) {
+      //   return "RESERVED"
+      // }
+
+      return "AVAILABLE";
     },
   },
   File: {
