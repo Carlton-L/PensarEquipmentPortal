43deab500a419adccabf47703a9aa33d48b7d7ee
Carlton Lindsay <carlton.l@outlook.com>
2021-05-25 12:53:10 -0700

    Tutorial

--- FileUpload/index.js
+++ FileUpload/index.js
@@ -1,13 +1,45 @@
+const {
+  StorageSharedKeyCredential,
+  ContainerSASPermissions,
+  generateBlobSASQueryParameters,
+} = require("@azure/storage-blob");
+const { extractConnectionStringParts } = require("./utils.js");
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const permissions = "c";
+  const container = "images";
+  context.res = {
+    body: generateSasToken(
+      process.env.AzureWebJobsStorage,
+      container,
+      permissions
+    ),
+  };
+  context.done();
+};
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+function generateSasToken(connectionString, container, permissions) {
+  const { accountKey, accountName, url } =
+    extractConnectionStringParts(connectionString);
+  const sharedKeyCredential = new StorageSharedKeyCredential(
+    accountName,
+    accountKey.toString("base64")
+  );
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  var expiryDate = new Date();
+  expiryDate.setHours(expiryDate.getHours() + 2);
+
+  const sasKey = generateBlobSASQueryParameters(
+    {
+      containerName: container,
+      permissions: ContainerSASPermissions.parse(permissions),
+      expiresOn: expiryDate,
+    },
+    sharedKeyCredential
+  );
+
+  return {
+    sasKey: sasKey.toString(),
+    url: url,
+  };
 }
