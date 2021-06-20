1f8a5c82e40782cb2931610e97a53041790e7285
Carlton Lindsay <carlton.l@outlook.com>
2021-06-20 13:15:02 -0700

    Update imgur.js

--- api/src/datasources/imgur.js
+++ api/src/datasources/imgur.js
@@ -7,17 +7,40 @@ class ImgurAPI extends RESTDataSource {
   }
 
   willSendRequest(request) {
-    request.headers.set("Content-Type", `application/x-www-form-urlencoded`);
-    request.headers.set(
-      "Authorization",
-      `Client-ID ${process.env.IMGUR_CLIENT_ID}`
-    );
+    request.mode = "no-cors";
+    request.cache = "no-cache";
     console.log(request);
+    console.log(request.headers);
   }
 
   async uploadImageFromUrl(url) {
     const formData = `image=${url}&album=${process.env.IMGUR_ALBUM_DELETE_HASH}&type=url`;
-    return this.post("upload", formData);
+    return this.post(`upload`, formData, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+      },
+    });
+  }
+
+  async changeImageInfo(hash, title, description) {
+    let formData;
+    if (title && description) {
+      formData += `title=${title}&description=${description}`;
+    } else if (title) {
+      formData += `title=${title}`;
+    } else if (description) {
+      formData += `description=${description}`;
+    } else {
+      return "No Change";
+    }
+
+    return this.post(`image/${hash}`, formData, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+      },
+    });
   }
 }
 
